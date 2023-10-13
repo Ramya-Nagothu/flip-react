205fca9fcd01a2c5f96daed808e307c3d6ba91a5
Ramya-Nagothu <ramyanagothu21@gmail.com>
2023-10-13 15:16:49 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import Searchbar from './components/Searchbar.js';
+import Menu from './components/Menu.js';
+import Cards from './components/Cards.js';
+import Slide from './components/Slide.js';
+import './css/bootstrap.css';
+import Forms from './components/Forms';
+import Prod from './components/Prod.js';
+
+function App() {
+  return (
+   <>
+   <div className='container-fluid'>
+        <div className='row'>
+                <Searchbar/>
+                <Menu/>
+                <Slide/>
+                <Cards/>
+                <Prod/>
+                <Forms/>
+        </div>
+   </div>
+   </>
+  );
+}
+
+export default App;
+
---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import banner from '../images/ban1.png'
+export default function Banner() {
+  return (
+   <>
+   <div className='row'>
+      <img src={banner}/>
+   </div>
+   </>
+  )
+}
