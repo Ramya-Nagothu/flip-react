814cb36c6f5dec4fa95150697af42ff4fefe3312
Ramya-Nagothu <ramyanagothu21@gmail.com>
2023-10-14 10:17:51 +0530

    change

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <div className='row'>
                 <Searchbar/>
                 <Menu/>
+                
                 <Slide/>
                 <Cards/>
                 <Prod/>
